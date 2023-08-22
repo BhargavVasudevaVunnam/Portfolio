83d24415ccb0359d7cff65c33667cc8c11ed71cc
BhargavVasudevaVunnam <vvbhargavchowdary@gmail.com>
2023-08-22 00:53:29 -0400

    First Page Draft

---
+++ src/Icons.js
@@ -0,0 +1,22 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import resume from './assets/Bhargav_Resume.pdf';
+import { FaFileDownload } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa';
+import  './Icons.css';
+function Icons(){
+return <div className="icons">
+    <a className="icon" href="https://www.linkedin.com/in/bhargav-chowdary-696a0417a/">
+       <FaLinkedin className="icon" size={25}/>
+    </a>
+    <a className="icon" href='https://github.com/BhargavVasudevaVunnam'>
+       <FaGithub className="icon" size={25}/>
+    </a>
+    <a className="icon" download="Bhargav_Vunnam_Resume.pdf"  href={resume}>
+       <FaFileDownload className="icon" size={19}/>
+    </a>
+    <a href="tel:1234567890">
+        <FaPhone className="icon" size={19} /> {/* Adjust the size as needed */}
+    </a>
+</div>
+}
+export default Icons;
--- src/Page1.js
+++ src/Page1.js
@@ -1,7 +1,29 @@
 import './Page1.css';
+import image from './assets/rot.JPG'
+import Icons from './Icons';
 function Page1(){
     return <div id="Introduction">
-        <p>First</p>
-    </div>
+                <div id="header">
+                  <p id="Title">Bhargav Vasudeva Vunnam<br/><span id="subheading">Full Stack Developer</span></p>
+                  <div id="imagecontainer">
+                  <img id="cover" src={image} alt="OOPS check internet"></img>
+                  </div>
+                  <div id="iconsContainer">
+                    <Icons></Icons>
+                  </div>
+                </div>
+                <div id="body">
+                   <div id="CrossBlackBoard">
+                        <p id="BlackText">
+                        <span id="chalk">Hi Bhargav Here, 
+                        <br/>
+                        <br/>
+                        I am a Developer with about 2 years of Professional experience in Full Stack Java Development. I like to solve analytical problems and deliver efficient solutions. Passionate towards different fields of Computer Science. Worked on projects with ReinForcement Learning, Machine Learning, Artificial Intelligence, Bio Informatics as well. <br/><br/>
+                        Have a myriad of languages and Frameworks in the palette obtained through experience. Would require very little overhead in getting started with new environments.  Personality wise I am an innovative, motivated individual.
+                        </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
 }
 export default Page1;
